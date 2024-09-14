271ba20279b0594abe428aac005d232bc7197ba6
Seth Holland <holland_s91@yahoo.com>
2024-09-14 16:42:34 -0500

    Simplify getting project list

--- lua/dotnet-plugin/project_picker.lua
+++ lua/dotnet-plugin/project_picker.lua
@@ -5,42 +5,13 @@ local conf = require("telescope.config").values
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 
-local function get_projects()
-  local sln_files = vim.fn.glob(vim.fn.getcwd() .. "/**/*.sln", false, true)
-  if #sln_files == 0 then
-    print("No .sln file found in the current directory or its subdirectories.")
-    return {}
-  end
-
-  local sln_file = sln_files[1]
-  local handle = io.open(sln_file, "r")
-  if not handle then
-    print("Failed to open .sln file.")
-    return {}
-  end
-
-  local relative_paths = {}
-  for line in handle:lines() do
-    local project_path = line:match('Project%([^)]+%)%s*=%s*"[^"]*"%s*,%s*"([^"]*)"')
-    if project_path then
-      local full_path = vim.fn.fnamemodify(sln_file, ":h") .. "/" .. project_path
-      table.insert(relative_paths, vim.fn.fnamemodify(full_path, ":~:."))
-    end
-  end
-  handle:close()
-
-  return relative_paths
-end
-
 local function pick_projects(opts, continuation)
   opts = opts or {}
 
   pickers.new(opts, {
     prompt_title = "Projects",
 
-    finder = finders.new_table({
-      results = get_projects(),
-    }),
+    finder = finders.new_oneshot_job({"sh", "-c", "dotnet sln list | tail -n +3"}, {}),
 
     sorter = conf.generic_sorter(opts),
     previewer = conf.file_previewer(opts),
