fbbac77ce484f9a30b1973bcea6b78b68ca45f1a
Seth Holland <holland_s91@yahoo.com>
2024-09-14 20:18:06 -0500

    Clean up file names

--- lua/dotnet-plugin/init.lua
+++ lua/dotnet-plugin/init.lua
@@ -1,6 +1,6 @@
 return {
-  dotnet_nuget_add = require("dotnet-plugin.dotnet_nuget_add"),
-  dotnet_nuget_remove = require("dotnet-plugin.dotnet_nuget_remove"),
-  dotnet_nuget_update = require("dotnet-plugin.dotnet_nuget_update"),
-  dotnet_reference_remove = require("dotnet-plugin.dotnet_reference_remove"),
+  dotnet_nuget_add = require("dotnet-plugin.nuget_add"),
+  dotnet_nuget_remove = require("dotnet-plugin.nuget_remove"),
+  dotnet_nuget_update = require("dotnet-plugin.nuget_update"),
+  dotnet_reference_remove = require("dotnet-plugin.reference_remove"),
 }
