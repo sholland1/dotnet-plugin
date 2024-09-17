bd0353f2361bfe50468a62a14ee56668eccf1714
Seth Holland <holland_s91@yahoo.com>
2024-09-16 21:20:44 -0500

    Sort commands in init.lua

--- lua/dotnet-plugin/init.lua
+++ lua/dotnet-plugin/init.lua
@@ -1,10 +1,10 @@
 return {
   plugin_command_list = require("dotnet-plugin.plugin_command_list"),
+  dotnet_new_item = require("dotnet-plugin.new_item"),
+  dotnet_new_project = require("dotnet-plugin.new_project"),
   dotnet_nuget_add = require("dotnet-plugin.nuget_add"),
   dotnet_nuget_remove = require("dotnet-plugin.nuget_remove"),
   dotnet_nuget_update = require("dotnet-plugin.nuget_update"),
   dotnet_reference_remove = require("dotnet-plugin.reference_remove"),
   dotnet_sln_project_remove = require("dotnet-plugin.project_remove"),
-  dotnet_new_project = require("dotnet-plugin.new_project"),
-  dotnet_new_item = require("dotnet-plugin.new_item"),
 }
