bbe3a29079380b30d73efa1210e4c344ea3caa25
Seth Holland <holland_s91@yahoo.com>
2024-09-20 16:34:33 -0500

    Add other commands to telescope extension registration

--- lua/telescope/_extensions/dotnet-plugin.lua
+++ lua/telescope/_extensions/dotnet-plugin.lua
@@ -1,6 +1,17 @@
+local dotnet = require("dotnet-plugin")
+
 return require("telescope").register_extension({
   exports = {
-    ["dotnet-plugin"] = require("dotnet-plugin").plugin_command_list,
-    dotnet_command_list = require("dotnet-plugin").plugin_command_list,
+    ["dotnet-plugin"] = dotnet.plugin_command_list,
+    command_list = dotnet.plugin_command_list,
+    new_item = dotnet.dotnet_new_item,
+    new_project = dotnet.dotnet_new_project,
+    nuget_add = dotnet.dotnet_nuget_add,
+    nuget_remove = dotnet.dotnet_nuget_remove,
+    nuget_update = dotnet.dotnet_nuget_update,
+    reference_add = dotnet.dotnet_reference_add,
+    reference_remove = dotnet.dotnet_reference_remove,
+    sln_project_add = dotnet.dotnet_sln_project_add,
+    sln_project_remove = dotnet.dotnet_sln_project_remove,
   },
 })
